9806e78284759d1e37f0bd6fe15d0d9dd9612b01
Antinew <2357729423@qq.com>
2022-11-17 01:30:36 +0800

    1

---
+++ multiThreading/ThreadingAdvance/拓展/AsyncLocal测试.cs
@@ -0,0 +1,45 @@
+﻿using Xunit.Abstractions;
+
+namespace ThreadingAdvance.拓展;
+
+public class AsyncLocal测试
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+    AsyncLocal<int> _num = new AsyncLocal<int>();
+    ThreadLocal<int> _num2 = new ThreadLocal<int>();
+
+    public AsyncLocal测试(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+
+    [Fact]
+    async Task Show()
+    {
+        async Task Work()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                _num.Value++;
+                _num2.Value++;
+            }
+            _testOutputHelper.WriteLine(_num.Value.ToString());
+            _testOutputHelper.WriteLine(_num2.Value.ToString());
+
+            await Task.Delay(100);
+            for (int i = 0; i < 100000; i++)
+            {
+                _num.Value++;
+                _num2.Value++;
+            }
+            _testOutputHelper.WriteLine(_num.Value.ToString());
+            _testOutputHelper.WriteLine(_num2.Value.ToString());
+        }
+
+        await Work();
+
+        _testOutputHelper.WriteLine(_num.Value.ToString());
+        _testOutputHelper.WriteLine(_num2.Value.ToString());
+    }
+}
