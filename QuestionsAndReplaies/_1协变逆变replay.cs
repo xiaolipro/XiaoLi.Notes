c24aaba8e155fc75deee05221f8b02dd0cac8b1d
xiaolipro <2357729423@qq.com>
2022-12-28 18:06:31 +0800

    1

---
+++ QuestionsAndReplaies/_1协变逆变replay.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using Xunit.Abstractions;
+
+namespace QuestionsAndReplaies;
+
+public class _1协变逆变replay
+{
+    private static ITestOutputHelper _testOutputHelper;
+
+    public _1协变逆变replay(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    
+    //https://learn.microsoft.com/zh-cn/search/?scope=.NET&terms=%E5%8D%8F%E5%8F%98%E9%80%86%E5%8F%98
+
+    /* Variant 变种
+     * Covariance 协变：儿子-》父亲
+     * Contravariance 逆变：父亲-》儿子
+     * 限制：只有接口类型和委托类型才能具有 Variant 类型参数。 接口或委托类型可以同时具有协变和逆变类型参数。
+     */
+    
+    [Fact]
+    public void 协变()
+    {
+        var son = new Son();
+        Father father = son;
+        
+        var sonForPeople = new People<Son>();
+        sonForPeople.Think();
+        IPeople<Father> fatherForPeople = sonForPeople;
+        fatherForPeople.Think();
+    }
+    
+    // [Fact]
+    // public void 逆变()
+    // {
+    //     object[] array = new String[10];  
+    //     var father = new Father();
+    //
+    //     var fatherForPeople = new People<Father>();
+    //     fatherForPeople.Think(father);
+    //     IPeople<Son> sonForPeople = fatherForPeople;
+    //     sonForPeople.Think(new Son());
+    // }
+
+    class Father
+    {
+    }
+
+    class Son : Father
+    {
+    }
+
+    class People<T> : IPeople<T>
+    {
+        public void Think()
+        {
+            _testOutputHelper.WriteLine(typeof(T).Name + "在思考");
+        }
+    }
+
+    interface IPeople<out T>
+    {
+        void Think();
+    }
+}
---
+++ QuestionsAndReplaies/_2Span和Memory.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Xunit.Abstractions;
+
+namespace QuestionsAndReplaies;
+
+public class _2Span和Memory
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public _2Span和Memory(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    
+    
+    [Fact]
+    void SPAN()
+    {
+        Span<int> span = stackalloc int[100000];
+
+        span.Slice(0, 100);
+        var arr = new int[100000];
+        var t = arr[1..2];
+    }
+
+    [Fact]
+    void SPAN陷阱()
+    {
+        Span<int> span = stackalloc int[100000];
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        for (int ctr = 0; ctr < span.Length; ctr++)
+            span[ctr] = span[ctr] * span[ctr]/3;
+        stopwatch.Stop();
+        _testOutputHelper.WriteLine(stopwatch.Elapsed.TotalNanoseconds.ToString());
+    }
+}
